293d70ae335b5284cb6fc148da1afcca8ea11daa
sengulhamza <sengulhamza@icloud.com>
2022-12-30 16:27:42 +0300

    tcp server and client added with using pthread

---
+++ tcp_socket/src/tcp_sock_client.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define TCP_SOCK_CLIENT_ADDR "127.0.0.1"
+#define TCP_SOCK_CLIENT_PORT 1234
+#define TCP_SOCK_CLIENT_BUFFER_SIZE 1024
+
+static void *tcp_sock_thread_send(void *cfd)
+{
+    char tmp[TCP_SOCK_CLIENT_BUFFER_SIZE] = {"A"};
+    int c = *((int *)cfd);
+    while (1) {
+        send(c, tmp, TCP_SOCK_CLIENT_BUFFER_SIZE, 0);
+        printf("        send ok\n");
+        sleep(2);
+    }
+}
+
+static void *tcp_sock_thread_recv(void *cfd)
+{
+    char tmp[TCP_SOCK_CLIENT_BUFFER_SIZE];
+    int d = *((int *)cfd);
+    while (1) {
+        int idata = 0;
+        idata = recv(d, tmp, TCP_SOCK_CLIENT_BUFFER_SIZE, 0);
+        if (idata > 0) {
+            printf("%s \r\n", tmp);
+        }
+    }
+}
+
+int main()
+{
+    struct sockaddr_in serv;
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1) {
+        perror("socket");
+        return -1;
+    }
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(TCP_SOCK_CLIENT_PORT);
+    inet_pton(AF_INET, TCP_SOCK_CLIENT_ADDR, &serv.sin_addr);
+    int ct = connect(sfd, (struct sockaddr *)&serv, sizeof(serv));
+    if (ct == -1) {
+        perror("connect");
+        return -1;
+    }
+    while (1) {
+        pthread_t tid1, tid2;
+        pthread_create(&tid1, NULL, tcp_sock_thread_send, &sfd);
+        pthread_create(&tid2, NULL, tcp_sock_thread_recv, &sfd);
+        pthread_join(tid1, NULL);
+        pthread_join(tid2, NULL);
+    }
+    return 0;
+}
